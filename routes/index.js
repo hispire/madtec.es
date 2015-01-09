e248b6f169a0ecbecf90fcf6ca87679fc1067d6a
Giorgix <jackosinapsis@gmail.com>
2015-01-09 18:23:02 +0100

    created branch and view start-project

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Madtec' });
 });
 
+router.get('/start-project', function(req, res) {
+  res.render('start-project');
+});
+
 module.exports = router;
