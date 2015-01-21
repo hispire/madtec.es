078b25183dd393437a5d87d4385b4829c19164c9
Giorgix <jackosinapsis@gmail.com>
2015-01-21 15:17:35 +0100

    added basic email config and transport

---
+++ config/email.js
@@ -0,0 +1,40 @@
+var nodemailer = require('nodemailer');
+
+this.transport = nodemailer.createTransport({
+    //service: 'Gmail',
+    host: "thsr1.supercp.com",
+    secure: true,
+    port: 465,
+    auth: {
+      user: "projects@madtec.es",
+      pass: "DaVinci5260"
+    }
+  });
+
+this.mailOptions = function (name, email, company, project_type, budget, timeframe, details) {
+  var mailOpts = {
+      from: company + ' <' + email + '>' ,
+      to: 'projects@madtec.es',
+      //replace it with id you want to send multiple must be separated by ,(comma)
+      subject: project_type,
+      //generateTextFromHTML: true,
+      text: "From: "+ name + "\n" +
+	    "Company: "+ company + "\n" +
+	    "Email: "+ email + "\n" +
+	    "Project: "+ project_type + "\n" +
+	    "Budget: "+ budget + "\n" +
+	    "Timeframe: "+ timeframe + "\n" +
+	    "Details: \n\n"+ details,
+      html: "<p><b>From: </b>" + name + 
+	    "</p> <p><b>Company: </b>"+ company +
+	    "</p> <p><b>Email: </b>"+ email +
+	    "</p> <p><b>Project: </b>"+ project_type +
+	    "</p> <p><b>Budget: </b>"+ budget +
+	    "</p> <p><b>Timeframe: </b>"+ timeframe +
+	    "</p> <h4>Details: </h4><p>"+ details + "</p>"
+    
+
+  
+  }
+    return mailOpts;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var email = require('.././config/email.js')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,6 +13,20 @@ router.get('/services', function(req, res) {
 
 router.get('/start-project', function(req, res) {
   res.render('start-project');
+});
+
+router.post('/start-project', function(req, res) {
+  email.transport.sendMail(email.mailOptions(req.body.name, req.body.email, req.body.company, 
+                                             req.body.project_type, req.body.budget, req.body.timeframe, 
+                                             req.body.project_details), 
+    function(err) {
+    if(err) {
+      res.status(500);
+      res.send('Error sending email: ' + err);
+    } else {
+      res.send('Email sent!');
+    }
+  }) 
 });
 
 module.exports = router;
